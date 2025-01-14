e226718f9941c52cf36c1501d4df2e307bca803e
udaykiet <90373947+udaykiet@users.noreply.github.com>
2025-01-14 08:15:59 +0530

    Delete notification-service directory

--- notification-service/src/main/java/com/ups/notification_service/repository/NotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ups.notification_service.repository;
-
-import com.ups.notification_service.entity.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NotificationRepository extends JpaRepository<Notification , Long> {
-}
